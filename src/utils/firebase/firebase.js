0ea6869954e0c636c147046933c94af3b0de2601
Abishai Yapélé <abishaiyp.dev@gmail.com>
2022-05-16 09:15:27 +0000

    Setting up the firestore database configuration

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -6,13 +6,15 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
-  onAuthStateChanged
+  onAuthStateChanged,
 } from 'firebase/auth'
 import {
   getFirestore,
   doc,
   getDoc,
-  setDoc
+  setDoc,
+  collection,
+  writeBatch
 } from 'firebase/firestore'
 
 
@@ -89,3 +91,18 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
 export const signOutUser = async () => await signOut(auth)
 export const onAuthStateChangeListenner = (callback) => onAuthStateChanged(auth, callback)
+
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = collection(db, collectionKey)
+  const batch = writeBatch(db)
+
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase())
+    batch.set(docRef, object)
+  })
+
+  await batch.commit()
+  console.log('done')
+}
